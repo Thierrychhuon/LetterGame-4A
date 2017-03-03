7ce41af8555210552323f46f5c78f1968a852879
neitge <clemntinho@hotmail.com>
2017-03-03 23:34:56 +0100

    window

---
+++ LetterGame/src/main/java/Front/Window.java
@@ -0,0 +1,98 @@
+package Front;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import fr.esiea.chhuondaniere.jeu.LetterPot;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
+
+public class Window {
+private JFrame frame;
+	
+	public Window(){
+		this.frame = new JFrame();
+		this.frame.setContentPane(buildContentPane());
+		this.frame.setTitle("Scrable"); 
+		this.frame.setSize(600,400); 
+		this.frame.setVisible(true);
+		this.frame.setLocationRelativeTo(null);
+		this.frame.setResizable(false); 
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+	private JPanel buildContentPane(){
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.gray);
+		GridLayout position = new GridLayout(3, 4, 20, 20);
+		panel.setLayout(position);
+		
+		JButton passer=new JButton("Passed");
+		passer.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent event){				
+		        
+		      }
+		    });
+		
+		JButton nouveau=new JButton("New");
+		nouveau.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent event){				
+		        
+		      }
+		    });
+		
+		JButton voler=new JButton("Steal");
+		voler.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent event){				
+		        
+		      }
+		    });
+		
+		JButton modifier=new JButton("custom");
+		modifier.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent event){				
+		        
+		      }
+		    });
+		
+		JButton valider=new JButton("ok");
+		modifier.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent event){				
+		        
+		      }
+		    });
+		
+		JLabel potcommun=new JLabel();
+		String printletter="";
+		potcommun.setText(LetterPot.CommonPot.forEach(letter ->));
+		
+		
+		JLabel textplayer1=new JLabel();
+		JLabel action=new JLabel();
+		JLabel reponse=new JLabel();
+		
+		JLabel textplayer2=new JLabel();
+		JLabel currentuplayer=new JLabel();
+		JTextArea word=new JTextArea();
+		panel.add(passer);
+		panel.add(nouveau);
+		panel.add(voler);
+		panel.add(modifier);
+		panel.add(textplayer1);
+		panel.add(potcommun);
+		panel.add(textplayer2);
+		panel.add(currentuplayer);
+		panel.add(action);
+		panel.add(word);
+		panel.add(valider);
+		panel.add(reponse); 
+		return panel;
+	}
+}
